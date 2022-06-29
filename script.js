d0b137762aad54f01d89161e3ee62fc0978b764c
frederic <flausson@normandiewebschool.fr>
2022-06-29 10:54:10 +0200

    result

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-/* fetch */ 
+console.log("loaded");
+
+function gen()
+{
+    console.log("click");
+
+    let truc = fetch("https://v1.api.amethyste.moe/generate/ddungeon",
+    { 
+        method: "POST",
+        headers: 
+        {
+            'Authorization': "Bearer " + token.value,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: new URLSearchParams({
+            'url': image.value,
+        })
+    });
+
+    truc.then(r => {
+        console.log(r);
+        let imageResult = r
+
+    });
+}
+
+function genplus() {
+    let b = document.getElementById(truc);
+    b.textContent = r.;
+}
